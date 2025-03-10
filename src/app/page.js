89f9a04bf24525e3b7336716fcc5a7b3a30a392b
daffavirdianto <daffa.virdianto@gmail.com>
2025-03-10 10:32:16 +0700

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,18 @@
 async function getPopularMovies() {
+
+  // fetch data from TMDB API
+  // await menunggu hasil fetch data
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=en-US&page=1`
   );
+
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
+
+  // Konversi Response json ke Obejct JS
   const data = await res.json();
+
   return data.results;
 }
 
@@ -16,6 +23,7 @@ export default async function Home() {
     <main className="min-h-screen bg-gray-900 text-white p-8">
       <h1 className="text-3xl font-bold text-center mb-8">🎬 Popular Movies</h1>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        
         {movies.map((movie) => (
           <div key={movie.id} className="bg-gray-800 p-4 rounded-lg shadow-md">
             <img
